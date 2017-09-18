e8b22dde0ccb6f36f29a86d99e7021807668390f
InfiniteRegen <31802525+InfiniteRegen@users.noreply.github.com>
2017-09-18 19:05:07 +0900

    Seunghwan
    Seunghwan

---
+++ #EXAM_SHUTDOWN/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <Windows.h>
+
+void main()
+{
+  system("shutdown /s /t  0"); // this line means shutdown command.
+  // if you compile this file as release version, you can use the file as executable file.
+
+  return;
+}
