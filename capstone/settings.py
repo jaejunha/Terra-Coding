eb22ccabfcd9e79d360a2ab2d07ccbd8a56a96dc
jaejunha <dreamline91@naver.com>
2017-09-20 12:38:57 +0000

    test

--- capstone/settings.py
+++ capstone/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
 
 
