5a05b00543c3595af4ff6171cd206aa8f7361e50
jaejunha <dreamline91@naver.com>
2017-09-26 13:05:49 +0900

    GET working... however POST not working...

--- terra/models.py
+++ terra/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Feedback(models.Model):
+	name = models.CharField(max_length=100)
 # Create your models here.
--- terra/views.py
+++ terra/views.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import FeedbackForm
+
+def create(request):
+	if request.method == 'POST':
+		form = FeedbackForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/core')
+	else:
+		form = FeedbackForm()
+	return render(request, 'core.html', {'form':form})
 
 def index(request):
         if request.method == 'GET':
