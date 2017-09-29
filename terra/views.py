b5a5feba20299c8cfd6658fabfcea2f3075af1e1
jaejunha <dreamline91@naver.com>
2017-09-29 23:24:42 +0900

    test for session error

--- terra/views.py
+++ terra/views.py
@@ -17,22 +17,26 @@ def intro(request):
 		return render(request, 'terra/templates/intro.html')
 
 def core(request):
-	if request.session['sid']:
-		cookies = {'JSESSIONID':request.session['sid']}
-		response = requests.get(USER, cookies=cookies)
-		soup = BeautifulSoup(response.text,'html.parser')
-		number = soup.find_all('td')[1].string
-		picture = 'http://job.ajou.ac.kr'+requests.get(PIC+number).text.split(' ')[2][5:].split('?')[0]
+	try:
+		if request.session['sid']:
+			cookies = {'JSESSIONID':request.session['sid']}
+			response = requests.get(USER, cookies=cookies)
+			soup = BeautifulSoup(response.text,'html.parser')
+			number = soup.find_all('td')[1].string
+			picture = 'http://job.ajou.ac.kr'+requests.get(PIC+number).text.split(' ')[2][5:].split('?')[0]
 #		for a in soup.find_all('td'):
 #			print(str(a))
-		return render(request, 'terra/templates/core.html', {'picture':picture})
-	else:
+			return render(request, 'terra/templates/core.html', {'picture':picture})
+	except Exception:
+		print(request.session['sid'])
+
 		return render(request, 'terra/templates/core.html')
 
 def index(request):
         return render(request, 'terra/templates/index.html')
 
 def login(request):
+	name = ''
 	if request.method == 'POST':
 		name = request.POST.get('id', '')
 		pwd = request.POST.get('pwd','')
@@ -42,10 +46,11 @@ def login(request):
 			response = requests.post(LOGIN, data=data)
 			status = json.loads(response.text)['response']
 			if status == 'OK':
-				request.session['name'] = name
+				request.session.flush()
 				for c in response.cookies:
 					if c.name == 'JSESSIONID':
 						request.session['sid']=c.value
+				print(request.session['sid'])
 				return HttpResponseRedirect(reverse('core'))
 	return render(request, 'terra/templates/index.html', {'name': name})
 
