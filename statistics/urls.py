d3383a249217cb67ee8fb937e0807ba80f167237
jaejunha <dreamline91@naver.com>
2017-10-02 16:32:52 +0900

    apply gitignore

--- statistics/urls.py
+++ statistics/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
-	url(r'^', include('intro.urls')),
-	url(r'^terra', include('terra.urls')),
- 	url(r'^coding', include('coding.urls')),
-	url(r'^statistics', include('statistics.urls')),
-	url(r'^info', include('info.urls')),
-	url(r'^rank', include('rank.urls')),
+	url(r'^$', views.index, name='index'),
 ]
