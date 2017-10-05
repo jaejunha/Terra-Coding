e70214e4e633eda3484bd4056b79831e8631a8af
InfiniteRegen <zeroback10@ajou.ac.kr>
2017-10-05 23:59:25 +0900

    (new: compile_interface_real_time), (fixed:sourceEdit.html redirection)

---
+++ refer/static/js/loadDiv.js
@@ -0,0 +1,5 @@
+function loadDiv(url){
+	var element=document.getElementById("content");
+        element.innerHTML="<iframe src=\""+url+"\" width=\"100%\" height=\"100%\" ></iframe>";                          
+}
+
---
+++ refer/static/js/move.js
@@ -0,0 +1,24 @@
+function login_move(error){
+	var screenWidth = window.innerWidth;
+	var screenHeight = window.innerHeight;
+
+	if(error == ''){
+		var login = document.getElementById('login');
+	}else{
+		var login = document.getElementById('login_error');
+	}
+	var width = login.offsetWidth;
+	var height = login.offsetHeight;
+	var logo = document.getElementById('logo');
+
+	login.style.left = (screenWidth-width)/2+'px';
+	login.style.top = (screenHeight-height)/2+'px';
+
+	width = logo.offsetWidth;
+	logo.style.left = (screenWidth-width)/2+'px';
+	logo.style.top = login.offsetTop-height/2.5+'px';
+}
+
+function terra_move(){
+	$('#top').css('left', $(window).width() - $('#top').width() +'px' );
+}
