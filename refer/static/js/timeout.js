385b8440e27e021edc9ecdbb7f4686a722a58ef9
jaejunha <dreamline91@naver.com>
2017-10-06 20:32:37 +0900

    disappear effect

--- refer/static/js/timeout.js
+++ refer/static/js/timeout.js
@@ -2,10 +2,12 @@ function intro_timeout(){
   setTimeout(function() {
     $('#gradation').css('animation','disappear 2s');
     $('#gradation').css('animation-fill-mode','both');
+    $('#developer').css('animation','disappear2 2s');
+    $('#developer').css('animation-fill-mode','both');
   }, 1000);
 
   setTimeout(function() {
-     $('#developer').css('opacity','0');
+  //   $('#developer').css('opacity','0');
      $('#logo').css('animation','appear 3s');
      $('#logo').css('animation-fill-mode','both');
      $('#logo').css('animation-iteration-count','infinite');
