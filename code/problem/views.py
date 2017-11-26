71c0d773f925763177ae0eb07e7140ad1a10aaa6
jaejunha <dreamline91@naver.com>
2017-11-27 00:28:05 +0900

    ing multiple answer

--- code/problem/views.py
+++ code/problem/views.py
@@ -25,7 +25,11 @@ def list(request):
 			no = 1
 		Problem(no=no,name=title,desc=desc).save()
 		p = Problem.objects.get(no=no)
-		Solution(sNo=p,ex=example,sol=solution).save()
+
+		list_example = example.split('&')
+		list_solution = solution.split('&')
+		for i in range(0,len(list_example)-1):
+			Solution(sNo=p,ex=list_example[i],sol=list_solution[i]).save()
 	elif op == 'modify':
 		no = request.POST.get('no',0)
 
@@ -41,10 +45,31 @@ def list(request):
 		number = request.POST.get('number', '')
 		Problem.objects.get(no=number).delete()
 
-	result = Solution.objects.all()
+	r = Solution.objects.all()
 	list = []
-	for r in result:
-		print r.sNo.desc
-		list.append((str(r.sNo.no),r.sNo.name,r.sNo.desc,r.ex,r.sol))
+	list_example =[]
+	list_solution =[]
+	problem_no = -1
+	length = len(r)
 
+	if length == 1:
+		list.append((str(r[0].sNo.no),r[0].sNo.name,r[0].sNo.desc,r[0].ex,r[0].sol))
+		return render(request, 'problem/templates/list.html',{'list':list})
+
+	for i in range (0,length-2):
+		if (i+1) != (length-1):
+			list_example.append(r[i].ex)
+			list_solution.append(r[i].sol)
+			if r[i].sNo.no != r[i+1].sNo.no:
+				list.append((str(r[i].sNo.no),r[i].sNo.name,r[i].sNo.desc,list_example,list_solution))
+				list_example =[]
+				list_solution =[]
+		else:
+			if r[i].sNo.no == r[i+1].sNo.no:
+				list_example.append(r[i+1].ex)
+				list_solution.append(r[i+1].sol)
+				list.append((str(r[i+1].sNo.no),r[i+1].sNo.name,r[i+1].sNo.desc,list_example,list_solution))
+			else:
+				list.append((str(r[i].sNo.no),r[i].sNo.name,r[i].sNo.desc,list_example,list_solution))
+				list.append((str(r[i+1].sNo.no),r[i+1].sNo.name,r[i+1].sNo.desc,r[i+1].ex,r[i+1].sol))
 	return render(request, 'problem/templates/list.html',{'list':list})
